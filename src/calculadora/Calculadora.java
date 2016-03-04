773551b7c81b5c4b6eec68405afdfede2a92f2b5
christianbegines <christianbeginescarrasco@gmail.com>
2016-03-04 08:44:01 +0100

    quitar imports unused

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -10,9 +10,6 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import javax.swing.*;
 
 /**
